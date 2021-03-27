5d1d8dc90bf8681e03b9e5fc43f7f8a6321a5c68
Amankori2307 <amankori2307@gmail.com>
2021-03-27 13:51:41 +0530

    Added All Required Models

--- parkinglot/admin.py
+++ parkinglot/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, ParkingLot, Charges
+from .models import User, ParkingLot, Charges, Parking, Transaction
 # Register your models here.
 admin.site.register(User)
 admin.site.register(ParkingLot)
 admin.site.register(Charges)
+admin.site.register(Parking)
+admin.site.register(Transaction)
