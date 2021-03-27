106fd49119f9bd8c984b8ed424319d1e9abbeb10
Amankori2307 <amankori2307@gmail.com>
2021-03-27 12:11:41 +0530

    Created User Model

--- parkinglot/models.py
+++ parkinglot/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from .managers import UserManager
 
-# Create your models here.
+
+class UserTypeChoices(models.TextChoices):
+    PARKING_LOT = 0, "PARKING_LOT"
+    CUSTOMER = 1, "CUSTOMER"
+class User(AbstractUser):
+    username = None
+    email = None
+    first_name = None
+    last_name = None
+    mobile_regex = RegexValidator(regex=r"\d{10}", message="Invalid mobile format(only 10 digit mobile no is allowed)".title())
+    mobile = models.CharField(
+        max_length=10,
+        validators=[mobile_regex],
+        unique=True
+    )
+    user_type = models.IntegerField(
+        choices=UserTypeChoices.choices,
+        default=UserTypeChoices.CUSTOMER
+    )
+
+    USERNAME_FIELD = 'mobile'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.mobile
