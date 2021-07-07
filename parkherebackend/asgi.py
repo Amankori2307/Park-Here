f3b632386a4af54dd31783de1588187aeaaea548
amankori2307 <amankori2307@gmail.com>
2021-07-07 07:04:46 +0530

    Fixed Settings Error

--- parkherebackend/asgi.py
+++ parkherebackend/asgi.py
@@ -1,12 +1,14 @@
 # mysite/asgi.py
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
-
+import django
+# from decouple import config
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parkherebackend.settings')
+django.setup()
+
+import chat.routing
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
@@ -16,3 +18,17 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+# import os
+# import django
+# from decouple import config
+# from channels.routing import get_default_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'{config("PROJECT_NAME")}.settings')
+# django.setup()
+# application = get_default_application()
